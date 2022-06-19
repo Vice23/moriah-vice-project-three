771337b4fe816c5687f6ccf7d223eab47fa1e6b9
Vice23 <mvice045@gmail.com>
2022-06-19 17:41:25 -0400

    Added Bookshelf display/hide functionality

--- src/UserBookshelf.js
+++ src/UserBookshelf.js
@@ -6,8 +6,14 @@ import { useState, useEffect } from 'react';
 
 const UserBookshelf = () => {
 
-    // initialize the stateful variable
+    // initialize the stateful variables
     const [books, setBooks] = useState([]);
+    const [isDisplayed, setIsDisplayed] = useState(false);
+
+    const handleClick = () => {
+
+        setIsDisplayed(!isDisplayed);
+    }
 
     useEffect(() => {
         const database = getDatabase(firebase);
@@ -44,26 +50,63 @@ const UserBookshelf = () => {
         remove(dbRef);
     }
 
-    return(
-        <aside>
-            <h2>My Bookshelf</h2>
+    return (
+        <aside className="bookshelf">
+            <button
+                className="bookshelf-btn"
+                onClick={handleClick}
+            >
+                My Bookshelf
+            </button>
+
             {
-                books.map( (book) => {
-                    return(
-                        <li key={book.key}>
-                            <p>{book.bookObject.title}</p>
-                            <button onClick={() => { handleRemoveBook(book.key) }}>Remove</button>
-                        </li>
-                    )
-                    
-                })
+                isDisplayed
+            
+                ? <ul className="bookshelf-list">
 
+                    {
+                        books.map((book) => {
+                            return (
+                                <li
+                                    className="bookshelf-item"
+                                    key={book.key}
+                                >
 
+                                    <h3>{book.bookObject.title}</h3>
+                                    {
+                                        (book.bookObject.author_name) ?
+                                            <h4>By: {book.bookObject.author_name[0]}</h4>
+                                            : <h4> Author Unknown</h4>
+                                    }
 
+                                    {
+                                        (book.bookObject.publish_year) ?
+                                            <h5>Published: {book.bookObject.publish_year[0]}</h5>
+                                            : <h5> Year Published Unknown</h5>
+                                    }
+
+                                    {
+                                        (book.bookObject.id_amazon) ?
+                                            <p>
+                                                <a className="cta-bookshelf" href={"https://www.amazon.com/s?k=" + book.bookObject.id_amazon[0]}>Find Out More</a>
+                                            </p>
+                                            : (book.bookObject.id_librarything) ?
+                                                <p >
+                                                    <a className="cta-bookshelf" href={"https://www.librarything.com/work/" + book.bookObject.id_librarything[0]}>Find Out More</a>
+                                                </p>
+                                                : <p >
+                                                    <a className="cta-bookshelf" href={"https://www.google.com/search?tbm=bks&q=" + book.bookObject.title + " by " + book.bookObject.author_name}>Find Out More</a>
+                                                </p>
+                                    }
+                                    <button className="remove-btn" onClick={() => { handleRemoveBook(book.key) }}>Remove</button>
+                                </li>
+                            )
+
+                        })
+                    }
+                </ul>
+                : null
             }
-
-        
-        
 
         </aside>
     )
